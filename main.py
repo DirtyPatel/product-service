293aa1da17cb6603cb2ca6a8cc1c36e2e5fc102e
Dharti Patel <pate0323@algonquinlive.com>
2025-09-28 16:23:09 -0400

    edited main.py

--- main.py
+++ main.py
@@ -18,5 +18,4 @@ def get_products():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 3030))
-    # IMPORTANT: use "main:app" here so reload works
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
